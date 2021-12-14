10936995febf13cb1464cf0dc52b801ed55df928
unknown <gayoyawalkar@gmail.com>
2021-12-14 08:12:17 +0530

    Added:Rest Controller Uc3

--- src/main/java/com/example/demo/controller/HomeWebController.java
+++ src/main/java/com/example/demo/controller/HomeWebController.java
@@ -1,9 +1,7 @@
 package com.example.demo.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class HomeWebController {
 	@GetMapping("/web")
