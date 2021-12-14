6a7743347a1efc6a89a0f9c2a316477b97e2d040
unknown <gayoyawalkar@gmail.com>
2021-12-14 08:14:49 +0530

    Added:Logger Uc2 Framework

---
+++ src/main/java/com/example/demo/Day2LoggerApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import com.example.demo.component.DemoBean;
+@SpringBootApplication
+public class Day2LoggerApplication {
+	public static final Logger logger=LoggerFactory.getLogger(Day2LoggerApplication.class);
+	public static void main(String[] args) {
+		logger.debug("Welcome to Spring concept Demo");
+		ApplicationContext context=SpringApplication.run(Day2LoggerApplication.class,args);
+		  logger.debug("Checking context:{}",context.getBean(DemoBean.class));
+		  DemoBean demoBean=context.getBean(DemoBean.class);
+		  System.out.println("Demo Bean="+demoBean.toString());	
+	}
+}
